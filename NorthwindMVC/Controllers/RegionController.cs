fdb9132e1d06cffd41b16906a2dce9ddfbef9a74
JanaEhab1 <janaqabil@gmail.com>
2025-09-15 11:28:11 +0300

    aaa

--- NorthwindMVC/Controllers/RegionController.cs
+++ NorthwindMVC/Controllers/RegionController.cs
@@ -1,17 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Mvc;
+using NorthwindMVC.Models;
 
 namespace NorthwindMVC.Controllers
 {
     public class RegionController : Controller
     {
-        // GET: Region
+        private NORTHWINDEntities db = new NORTHWINDEntities();
+        public RegionController()
+        {
+            // Disable EF proxy objects to avoid circular reference in JSON
+            db.Configuration.ProxyCreationEnabled = false;
+        }
         public ActionResult Index()
         {
-            return View();
+            var regions = db.Regions.ToList();
+            return View(regions);
+        }
+
+        [HttpGet]
+        public JsonResult GetRegion(int id)
+        {
+            var region = db.Regions.FirstOrDefault(r => r.RegionID == id);
+            if (region != null)
+            {
+                return Json(new { success = true, data = region }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { success = false, message = "Region not found" }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+
+        [HttpPost]
+        public JsonResult UpdateRegion(int RegionId , string RegionDescription)
+        {
+            var region = db.Regions.FirstOrDefault(r => r.RegionID == RegionId);
+            if (region != null)
+            {
+                region.RegionDescription = RegionDescription;
+                try
+                {
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Region updated successfully" });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false, message = ex.Message });
+                };
+            }
+            else
+            {
+                return Json(new { success = false, message = "Region not found" });
+            }
+
         }
     }
 }
---
+++ NorthwindMVC/Scripts/region-edit.js
@@ -0,0 +1,54 @@
+﻿$(document).on('click', '.edit-btn', function () {
+    var regionId = $(this).data('id');
+    console.log("Edit clicked for RegionID:", regionId);
+
+    $.ajax({
+        url: '/Region/GetRegion',
+        type: 'GET',
+        data: { id: regionId },
+        success: function (response) {
+           
+            if (response.success) {
+                $('#editRegionId').val(response.data.RegionID);
+                $('#editRegionDescription').val(response.data.RegionDescription);
+
+                var myModal = new bootstrap.Modal(document.getElementById('editRegionModal'));
+                myModal.show();
+            } else {
+                alert(response.message);
+            }
+        },
+        error: function () {
+            alert('Error fetching data');
+        }
+    });
+});
+
+$('#saveEditBtn').click(function () {
+    var regionData = {
+        RegionId: $('#editRegionId').val(),
+        RegionDescription: $('#editRegionDescription').val()
+    };
+
+    $.ajax({
+        url: '/Region/UpdateRegion',
+        type: 'POST',
+        data: regionData,
+        success: function (response) {
+            if (response.success) {
+                alert("Region updated successfully!");
+
+                var myModalEl = document.getElementById('editRegionModal');
+                var modal = bootstrap.Modal.getInstance(myModalEl);
+                modal.hide();
+
+                location.reload();
+            } else {
+                alert("Something went wrong!");
+            }
+        },
+        error: function () {
+            alert("Error while saving changes!");
+        }
+    });
+});
